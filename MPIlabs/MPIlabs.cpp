bb5736392c4b97592a91cf0f290977ec5634fd7f
4z0t <gastec139@gmail.com>
2022-10-10 23:32:29 +0300

    Добавьте файлы проекта.

---
+++ MPIlabs/MPIlabs.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+#include "mpi.h"
+
+int main(int argc, char** argv)
+{
+	int proc_num, proc_id;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+	MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
+	std::cout << "number of proc " << proc_num << "\tid " << proc_id << std::endl;
+	MPI_Finalize();
+
+	return 0;
+}
